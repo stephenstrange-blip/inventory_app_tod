a498f6fafe21aed04e362a10f1a83b1f4bfa43f1
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-05 13:24:54 +0800

    Chore: Add styling files and assets

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 
 const PORT = Number(process.env.PORT) || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
   console.log(`Click http://${process.env.HOST}:${PORT}`);
